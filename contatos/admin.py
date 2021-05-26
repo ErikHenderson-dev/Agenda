42bd91b99c7be08f57a876785663d17dabaddac1
ErikHenderson-dev <erikhenderson.dev@gmail.com>
2021-05-26 20:24:57 -0300

    incluindo opção para mostrar contato ou ocultar contato no admin, limite de contatos por pagina e paginação.

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Categoria, Contato
 # Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'mostrar')
+    list_display_links = ('nome', 'sobrenome')
+    list_per_page = 5
+    search_fields = ('nome', 'sobrenome')
+    list_editable = ('telefone', 'mostrar')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Contato
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 5)
+
+    page_number = request.GET.get('page')
+    contatos = paginator.get_page(page_number)
+
+    return render(request, 'contatos/index.html',{
+        'contatos': contatos
+    })
+
+def visualizar_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+
+    return render(request, 'contatos/visualizar_contato.html',{
+        'contato': contato
+    })
+
+
